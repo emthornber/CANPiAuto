f4d4472c9b1c4ab186fd5348645fa128fa214af9
Mark Thornber <mark.thornber@gmail.com>
2022-05-04 12:29:22 +0100

    Clean compile

--- src/tcpClientGridConnect.cpp
+++ src/tcpClientGridConnect.cpp
@@ -78,7 +78,7 @@ void tcpClientGridConnect::canMessage(int canid, const char *msg, int dlc) {
         byte h2, h1;
         int s;
         char frametype = 'N'; //should be N or R
-        char t[2];
+        char t[3];
         //set frame type
         if ((canid & CAN_RTR_FLAG) == CAN_RTR_FLAG) frametype = 'R';
 
